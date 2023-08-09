1748610c05bb12989e08de567e079621a8f8c85b
priv.dnak <agrathe636@gmail.com>
2023-08-09 15:06:30 +0200

    0.03

---
+++ java-se-practice/src/main/java/exercises/basic/ex6/DecimalComparator.java
@@ -0,0 +1,18 @@
+package exercises.basic.ex6;
+
+public class DecimalComparator {
+    public static void main(String[] args){
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175) == true);
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176) == false);
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0) == true);
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123) == false);
+    }
+    private static boolean areEqualByThreeDecimalPlaces(double number1, double number2){
+        if((int)(number1*1000) == (int)(number2*1000)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ java-se-practice/src/main/java/exercises/basic/ex7/EqualSumChecker.java
@@ -0,0 +1,17 @@
+package exercises.basic.ex7;
+
+public class EqualSumChecker {
+    public static void main(String[] args){
+        System.out.println(hasEqualSum(1, 1, 1) == false);
+        System.out.println(hasEqualSum(1, 1, 2) == true);
+        System.out.println(hasEqualSum(1, -1, 0) == true);
+    }
+    private static boolean hasEqualSum(int number1, int number2, int number3){
+        if((number1+number2) == number3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
